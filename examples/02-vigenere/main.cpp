45d4a54ba765f48fa9c7a8dbddb2ecf14b991f82
lyrahgames <lyrahgames@mailbox.org>
2021-11-23 00:04:15 +0100

    Add Vigenere example for known key length

---
+++ examples/02-vigenere/main.cpp
@@ -0,0 +1,76 @@
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+//
+#include <fmt/format.h>
+//
+#include <lyrahgames/log/log.hpp>
+#include <lyrahgames/options/options.hpp>
+//
+#include <lyrahgames/cipher/cipher.hpp>
+
+using namespace std;
+using namespace fmt;
+using namespace lyrahgames;
+
+using options::attachment;
+using options::flag;
+using options::option_list;
+option_list<flag<"help", "Print help message.", 'h'>,
+            flag<"quiet", "Disable printing of info.", 'q'>,
+            attachment<"input", "Provide file for decryption.", 'i'> >
+    my_options{};
+
+void print_help() {
+  for_each(my_options, [](auto& x) {
+    print("  {:<20}{:<40}\n", x.help(), x.description());
+  });
+}
+
+int main(int argc, char* argv[]) {
+  log::log log{};
+  parse({argc, argv}, my_options);
+
+  if (value<"help">(my_options)) {
+    print_help();
+    return 0;
+  }
+
+  if (value<"quiet">(my_options)) log.quiet = true;
+
+  if (value<"input">(my_options) == "") {
+    log.error("No file for decryption given.");
+    return -1;
+  }
+
+  // Open given file.
+  fstream file{value<"input">(my_options), ios::in};
+  if (!file) {
+    log.error(format("Failed to open '{}'.", value<"input">(my_options)));
+    return -1;
+  }
+
+  // Determine size of file.
+  file.seekg(0, ios::end);
+  const auto len = file.tellg();
+  file.seekg(0, ios::beg);
+
+  // Read content of file into string.
+  string content;
+  content.resize(len);
+  file.read(&content[0], len);
+
+  //
+  using block = cipher::ascii_block;
+  using cipher = cipher::vigenere<block>;
+  cipher::key_type key{};
+
+  log.info("Doing cryptanalysis for given file by counting frequencies.");
+  key = cipher::frequency_analysis(' ', 3, content);
+  log.info(format("Estimated Key = '{}'", (const char*)key.data()));
+  log.info("Decrypt the given file with the estimated key.");
+  cipher::decrypt(key, content);
+
+  cout << content << endl;
+}
--- lyrahgames/cipher/vigenere.hpp
+++ lyrahgames/cipher/vigenere.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <cassert>
+#include <map>
 #include <random>
 #include <vector>
 //
@@ -58,6 +60,28 @@ struct vigenere {
   static constexpr void decrypt(key_type key,
                                 generic::range<block> auto& data) {
     decrypt(key, data, data);
+  }
+
+  static constexpr auto frequency_analysis(
+      block most_frequent,
+      size_t key_size,
+      const generic::input_range<block> auto& text) -> key_type {
+    // Use (hash) map to count frequencies.
+    std::vector<std::map<block, int>> frequency(key_size);
+    // Count frequencies.
+    for (size_t k = 0; auto c : text) {
+      ++frequency[k][c];
+      k = (k + 1) % key_size;
+    }
+    key_type key(key_size);
+    for (size_t i = 0; i < key_size; ++i) {
+      const auto max_it =
+          std::max_element(begin(frequency[i]), end(frequency[i]),
+                           [](auto x, auto y) { return x.second < y.second; });
+      // Compute shift of most frequent block.
+      key[i] = max_it->first - most_frequent;
+    }
+    return key;
   }
 };
 
