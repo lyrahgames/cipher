700ddac6ed47a8d1b2c0a01b48d805e948caba46
lyrahgames <lyrahgames@mailbox.org>
2021-11-22 17:37:17 +0100

    Add program options to first example

--- examples/01-add-decrypt/main.cpp
+++ examples/01-add-decrypt/main.cpp
@@ -6,6 +6,7 @@
 #include <fmt/format.h>
 //
 #include <lyrahgames/log/log.hpp>
+#include <lyrahgames/options/options.hpp>
 //
 #include <lyrahgames/cipher/cipher.hpp>
 
@@ -13,19 +14,40 @@ using namespace std;
 using namespace fmt;
 using namespace lyrahgames;
 
+using options::attachment;
+using options::flag;
+using options::option_list;
+option_list<flag<"help", "Print help message.", 'h'>,
+            flag<"quiet", "Disable printing of info.", 'q'>,
+            attachment<"input", "Provide file for decryption.", 'i'> >
+    my_options{};
+
+void print_help() {
+  for_each(my_options, [](auto& x) {
+    print("  {:<20}{:<40}\n", x.help(), x.description());
+  });
+}
+
 int main(int argc, char* argv[]) {
   log::log log{};
-  log.quiet = true;
+  parse({argc, argv}, my_options);
 
-  if (argc != 2) {
+  if (value<"help">(my_options)) {
+    print_help();
+    return 0;
+  }
+
+  if (value<"quiet">(my_options)) log.quiet = true;
+
+  if (value<"input">(my_options) == "") {
     log.error("No file for decryption given.");
     return -1;
   }
 
   // Open given file.
-  fstream file{argv[1], ios::in};
+  fstream file{value<"input">(my_options), ios::in};
   if (!file) {
-    log.error(format("Failed to open '{}'.", argv[1]));
+    log.error(format("Failed to open '{}'.", value<"input">(my_options)));
     return -1;
   }
 
