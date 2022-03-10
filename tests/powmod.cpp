7e0b7c7a58c684e3b07e9cae468edc93dca1612a
lyrahgames <lyrahgames@mailbox.org>
2022-03-10 16:56:51 +0100

    Test modular exponentiation with GMP

--- tests/powmod.cpp
+++ tests/powmod.cpp
@@ -11,15 +11,28 @@
 using namespace std;
 using namespace lyrahgames::cipher;
 
-SCENARIO("Modular Exponentiation") {
+SCENARIO("Modular Exponentiation with 64-bit Unsigned Integers") {
   mt19937 rng{random_device{}()};
-
   constexpr int n = 10'0000;
   for (int i = 0; i < n; ++i) {
-    const size_t x = rng();
-    const size_t n = rng();
+    const size_t x = uint32_t(rng());
+    const size_t n = uint32_t(rng());
     size_t z = 1;
     for (size_t e = 0; e < 100; ++e) {
+      CHECK(powmod(x, e, n) == z);
+      z = (z * x) % n;
+    }
+  }
+}
+
+SCENARIO("Modular Exponentiation with GNU Multiple Precision") {
+  gmp_randclass rng{gmp_randinit_default};
+  constexpr int n = 1000;
+  for (int i = 0; i < n; ++i) {
+    const mpz_class x = rng.get_z_bits(512);
+    const mpz_class n = rng.get_z_bits(512);
+    mpz_class z = 1;
+    for (mpz_class e = 0; e < 100; ++e) {
       CHECK(powmod(x, e, n) == z);
       z = (z * x) % n;
     }
